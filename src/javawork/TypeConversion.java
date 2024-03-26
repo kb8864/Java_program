0b1cb56597420ab1b65ca91050a2fdfc4dc4bab3
kb8864 <yt7s858864@gmail.com>
2024-03-26 10:54:57 +0900

    文字から数字に型変換

--- src/javawork/TypeConversion.java
+++ src/javawork/TypeConversion.java
@@ -4,14 +4,16 @@ public class TypeConversion {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		1型変換
-		int x = 100;
-		String y = "円";
+        // 数字から文字に型変換
+        int num = 100;
+        String str = String.valueOf(num);
+        String result = str + "円";
+        System.out.println(result); 
 
-		String s = String.valueOf(x);
-		y = s + y; 
-	
-		System.out.println(y);
+        // 文字から数字に型変換
+        String numberStr = "200";
+        int convertedNum = Integer.parseInt(numberStr);
+        System.out.println(convertedNum + 50); 
 	
 	}
 
