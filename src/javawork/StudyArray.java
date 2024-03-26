fa83dd180906b514db989625d256456653eb0a08
kb8864 <yt7s858864@gmail.com>
2024-03-26 12:27:12 +0900

    配列の学習

---
+++ src/javawork/StudyArray.java
@@ -0,0 +1,16 @@
+package javawork;
+
+public class StudyArray {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] name = {"山田", "田中", "佐藤"};
+		double[] height = {170.2, 183.3, 175.7};
+		
+		for(int i = 0; i <= 2; i++) {
+			System.out.println(name[i] + ":" + height[i] + "cm");
+		}
+
+	}
+
+}
