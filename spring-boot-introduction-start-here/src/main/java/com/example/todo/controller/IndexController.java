2cc5e9faa69e3b38197d69f91f65518ed9f3c569
kb8864 <yt7s858864@gmail.com>
2024-04-02 15:12:00 +0900

    トップページの作成、タスク一覧画面の作成、ページ遷移

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/IndexController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/IndexController.java
@@ -1,11 +1,11 @@
-package controller;
+package com.example.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class IndexController {
-    //http://localhost:8080/にアクセス後、Helloworldを表示
+
     @GetMapping
     public String index(){
         return "index";
