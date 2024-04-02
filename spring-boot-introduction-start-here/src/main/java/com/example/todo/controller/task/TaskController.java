e4d86f66cd048ee61e2295ee5270637f68a18bce
kb8864 <yt7s858864@gmail.com>
2024-04-03 01:31:27 +0900

    複数のタスクを表示

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,14 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 
 public class TaskController {
     @GetMapping("/tasks")
 
     public String list(Model model){
-        var task = new TaskDTO(1L,  "Spring勉強", "TODOアプリケーションの作成", "TODO");
-        model.addAttribute("task",task);
+        var task1 = new TaskDTO(1L,  "Spring勉強", "TODOアプリケーションの作成", "TODO");
+
+        var task2 = new TaskDTO(2L,  "Springセキュリティーの勉強", "ログイン機能の実装", "TODO");
+
+        var taskList = List.of(task1, task2);
+
+        model.addAttribute("taskList",taskList);
         return "tasks/list";
     }
 }
