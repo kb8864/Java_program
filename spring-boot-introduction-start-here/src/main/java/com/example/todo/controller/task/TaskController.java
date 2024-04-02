9ca2e6655be45ad1b0ae8e4b2d28102485244642
kb8864 <yt7s858864@gmail.com>
2024-04-03 02:15:26 +0900

    サービス層を作ってビジネスロジックの分離

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,24 +1,19 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 
 public class TaskController {
+    private final TaskService taskService = new TaskService();
     @GetMapping("/tasks")
 
     public String list(Model model){
-        var task1 = new TaskDTO(1L,  "Spring勉強", "TODOアプリケーションの作成", "TODO");
 
-        var task2 = new TaskDTO(2L,  "Springセキュリティーの勉強", "ログイン機能の実装", "TODO");
-
-        var taskList = List.of(task1, task2);
-
-        model.addAttribute("taskList",taskList);
+        model.addAttribute("taskList",taskService.find());
         return "tasks/list";
     }
 }
