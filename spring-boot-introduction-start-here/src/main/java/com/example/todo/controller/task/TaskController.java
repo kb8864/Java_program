c36ebbdcd7b441aae1904f900d3d97f8ad3dbd75
kb8864 <yt7s858864@gmail.com>
2024-04-02 17:26:48 +0900

    javaからThymeleaf にデータを渡す、Thymeleaf に渡すデータを DTO にまとめる

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.todo.controller.task;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TaskController {
     @GetMapping("/tasks")
 
-    public String list(){
+    public String list(Model model){
+        var task = new TaskDTO(1L,  "Spring勉強", "TODOアプリケーションの作成", "TODO");
+        model.addAttribute("task",task);
         return "tasks/list";
     }
 }
